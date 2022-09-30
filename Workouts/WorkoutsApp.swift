5e36a1fcabc1fe545737279c5a8b770cafe91d5a
Wo-m <dom.thompson458@gmail.com>
2022-09-30 19:44:17 +1000

    Initial Commit

---
+++ Workouts/WorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutsApp.swift
+//  Workouts
+//
+//  Created by Dominic Thompson on 30/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
